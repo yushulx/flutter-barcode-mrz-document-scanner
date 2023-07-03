81d73fbca67ba10b8e16a5c803a4db3a37e5afc7
yushulx <lingxiao1002@gmail.com>
2023-07-03 13:08:06 +0800

    Updated

--- lib/camera/camera_manager.dart
+++ lib/camera/camera_manager.dart
@@ -241,7 +241,7 @@ class CameraManager {
   Future<void> toggleCamera(int index) async {
     // if (controller != null) controller!.dispose();
 
-    controller = CameraController(_cameras[index], ResolutionPreset.high);
+    controller = CameraController(_cameras[index], ResolutionPreset.medium);
     controller!.initialize().then((_) {
       if (!cbIsMounted()) {
         return;
