a9f08c33ced8ae87f4a0f49bfe3de78783717fd9
yushulx <lingxiao1002@gmail.com>
2024-05-06 11:18:57 +0800

    Added iOS dependencies

--- examples/native_camera/ios/Runner/AppDelegate.swift
+++ examples/native_camera/ios/Runner/AppDelegate.swift
@@ -1,8 +1,16 @@
 import UIKit
 import Flutter
+import DynamsoftBarcodeReader
+import AVFoundation
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+
+  private var flutterTextureRegistry: FlutterTextureRegistry?
+  private var cameraSession: AVCaptureSession?
+  private var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+  private let CHANNEL = "barcode_scan"
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
